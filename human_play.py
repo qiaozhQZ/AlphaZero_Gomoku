d942f9a7b5c5b8ae6db3682e5bf516367cb34733
qiaozhd2@gmail.com <qiaozhd2@gmail.com>
2021-10-12 18:28:18 +0000

    uncomment code of using pyTorch

--- human_play.py
+++ human_play.py
@@ -13,7 +13,7 @@ from mcts_pure import MCTSPlayer as MCTS_Pure
 from mcts_alphaZero import MCTSPlayer
 from policy_value_net_numpy import PolicyValueNetNumpy
 # from policy_value_net import PolicyValueNet  # Theano and Lasagne
-# from policy_value_net_pytorch import PolicyValueNet  # Pytorch
+from policy_value_net_pytorch import PolicyValueNet  # Pytorch
 # from policy_value_net_tensorflow import PolicyValueNet # Tensorflow
 # from policy_value_net_keras import PolicyValueNet  # Keras
 
@@ -49,7 +49,9 @@ class Human(object):
 def run():
     n = 5
     width, height = 8, 8
-    model_file = 'best_policy_8_8_5.model'
+#     model_file = 'best_policy_8_8_5.model'
+    model_file = 'best_policy_885_2_11000.model'
+
     try:
         board = Board(width=width, height=height, n_in_row=n)
         game = Game(board)
@@ -57,19 +59,20 @@ def run():
         # ############### human VS AI ###################
         # load the trained policy_value_net in either Theano/Lasagne, PyTorch or TensorFlow
 
-        # best_policy = PolicyValueNet(width, height, model_file = model_file)
-        # mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
+        best_policy = PolicyValueNet(width, height, model_file = model_file, use_gpu=True)
+        mcts_player = MCTSPlayer(best_policy.policy_value_fn, c_puct=5, n_playout=400)
 
         # load the provided model (trained in Theano/Lasagne) into a MCTS player written in pure numpy
-        try:
-            policy_param = pickle.load(open(model_file, 'rb'))
-        except:
-            policy_param = pickle.load(open(model_file, 'rb'),
-                                       encoding='bytes')  # To support python3
-        best_policy = PolicyValueNetNumpy(width, height, policy_param)
-        mcts_player = MCTSPlayer(best_policy.policy_value_fn,
-                                 c_puct=5,
-                                 n_playout=400)  # set larger n_playout for better performance
+#         try:
+#             policy_param = pickle.load(open(model_file, 'rb'))
+#         except:
+#             policy_param = pickle.load(open(model_file, 'rb'),
+#                                        encoding='bytes')  # To support python3
+#         best_policy = PolicyValueNetNumpy(width, height, policy_param)
+        
+#         mcts_player = MCTSPlayer(best_policy.policy_value_fn,
+#                                  c_puct=5,
+#                                  n_playout=400)  # set larger n_playout for better performance
 
         # uncomment the following line to play with pure MCTS (it's much weaker even with a larger n_playout)
         # mcts_player = MCTS_Pure(c_puct=5, n_playout=1000)
